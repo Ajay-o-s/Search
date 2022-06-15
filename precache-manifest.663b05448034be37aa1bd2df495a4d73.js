3a4b862dcfd84df71c0b64d3ce33488632e3692f
Ajay o s <akhubsmaster@gmail.com>
2022-06-15 09:40:47 +0000

    👨‍💻CODE BY 🕊️★⃝AJAY O S©️🧚‍♂️

---
+++ precache-manifest.663b05448034be37aa1bd2df495a4d73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a1c4cee8bd94ba67a4e4938da2f8e45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c43035b6aee3797795c",
+    "url": "/static/css/main.c238c3b7.chunk.css"
+  },
+  {
+    "revision": "aa61c133d906aa76605e",
+    "url": "/static/js/2.b2cd2dce.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b2cd2dce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c43035b6aee3797795c",
+    "url": "/static/js/main.e23dfe17.chunk.js"
+  },
+  {
+    "revision": "06694fbe4449f41af521",
+    "url": "/static/js/runtime-main.a0e3b103.js"
+  },
+  {
+    "revision": "f16919420331df17fd7e8d1597bfc2cf",
+    "url": "/static/media/background.f1691942.webp"
+  }
+]);
